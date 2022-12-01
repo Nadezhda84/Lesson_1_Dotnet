7697d872325f0fceef5312a121ebbde10b6e8daa
Dusha <nadezdafurman84@gmail.com>
2022-12-01 14:46:33 +0600

    some changes added

--- Example005_ConditionIfElse/Program.cs
+++ Example005_ConditionIfElse/Program.cs
@@ -1,12 +1,11 @@
 ﻿Console.WriteLine("Введите имя пользователя: ");
-string username = Console.ReadLine();
+string username = Console.ReadLine() ?? "";
 if (username.ToLower() == "маша")
 {
     Console.WriteLine("Ура, это же МАША");
 }
 else
 {
-    Console.WriteLine("Привет,");
-    Console.WriteLine(username);
+    Console.WriteLine($"Привет, {username}");    
 }
 
