9b96b7f0da7892952c9b336c4861e585479c5626
Dusha <nadezdafurman84@gmail.com>
2022-12-01 15:59:57 +0600

    Method array added

---
+++ Exemple010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 12, 18, 3, 4, 5, 6, 7, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // прерывает, когда нашел первый элемент
+    }
+    index++;
+}
